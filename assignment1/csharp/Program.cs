1b6ff1d5ae0f208fbe64f04bff1698857d4853f5
davidfeldner <davidfeldner12@gmail.com>
2024-09-04 17:57:16 +0200

    Add csharp code to assignment 1

---
+++ assignment1/csharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using intro1;
+
+Expr e = new Add(new CstI(17), new Var("z"));
+Console.WriteLine(e.toString());
+
+
+Expr e1 = new Add(new Mul(new CstI(2), new CstI(5)), new Var("z"));
+Console.WriteLine(e1.toString());
+
+Expr e2 = new Sub(new Add(new Var("x"), new CstI(5)), new Sub(new Var("x"), new CstI(2)));
+Console.WriteLine(e2.toString());
+
+Expr e3 = new Mul(new Mul(new Var("x"), new Mul(new Var("x"), new CstI(9))), new Sub(new Var("y"), new Mul(new Var("x"), new CstI(9))));
+Console.WriteLine(e3.toString());
+
+
+Expr e4 = new Add(new Add(new CstI(2), new CstI(0)), new Var("z"));
+Console.WriteLine(e4.simplify().toString());
+
+
+Expr e5 = new Add(new Add(new CstI(2), new CstI(0)), new Sub(new Var("z"), new CstI(0)));
+Console.WriteLine(e5.simplify().toString());
+
+Expr e6 = new Mul(new Add(new Add(new CstI(2), new CstI(0)), new Sub(new Var("z"), new CstI(0))), new CstI(0));
+Console.WriteLine(e6.simplify().toString());
---
+++ assignment1/csharp/intro1.cs
@@ -0,0 +1,144 @@
+using System.Reflection.Metadata.Ecma335;
+
+namespace intro1;
+
+
+
+abstract class Expr {
+    public abstract string toString();
+    public abstract int eval(List<(string, int)> env);
+    public abstract Expr simplify();
+}
+
+
+class CstI : Expr {
+    public int i;
+    public CstI (int newI) {
+        i = newI;
+    }
+
+    public override string toString() {
+        return $"{i}";
+    }
+
+    public override int eval(List<(string, int)> env) {
+        return i;
+    }
+
+    public override Expr simplify() {
+        return this;
+    }
+}
+
+class Var : Expr {
+    public string s;
+    public Var (string newS) {
+        s = newS;
+    }
+
+    public override string toString() {
+        return $"{s}";
+    }
+
+    public override int eval(List<(string, int)> env) {
+        return env.Find(t => t.Item1 == s).Item2;
+    }
+
+    public override Expr simplify() {
+        return this;
+    }
+}
+
+abstract class Binop : Expr {
+
+}
+
+class Add : Binop {
+    Expr _e1;
+    Expr _e2;
+    public Add (Expr e1, Expr e2) {
+        _e1 = e1;
+        _e2 = e2;
+    }
+    public override string toString() {
+        return $"{_e1.toString()}+{_e2.toString()}";
+    }
+
+    public override int eval(List<(string, int)> env) {
+        return _e1.eval(env) + _e2.eval(env);
+    }
+
+    public override Expr simplify() {
+        var s1 = _e1.simplify();
+        var s2 = _e2.simplify();
+
+        if (s1 is CstI i1 && s2 is CstI i2) { 
+            return new CstI (i1.i + i2.i); 
+        } else if (s1 is CstI i3 && i3.i == 0) {
+            return s2;
+        } else if (s2 is CstI i4 && i4.i == 0) {
+            return s1;
+        } else {
+            return new Add(s1, s2);
+        }
+    }
+}
+class Mul : Binop {
+    Expr _e1;
+    Expr _e2;
+    public Mul (Expr e1, Expr e2) {
+        _e1 = e1;
+        _e2 = e2;
+    }
+    public override string toString() {
+        return $"{_e1.toString()}*{_e2.toString()}";
+    }
+
+    public override int eval(List<(string, int)> env) {
+        return _e1.eval(env) * _e2.eval(env);
+    }
+
+    public override Expr simplify() {
+        var s1 = _e1.simplify();
+        var s2 = _e2.simplify();
+
+        if (s1 is CstI i1 && s2 is CstI i2) { 
+            return new CstI (i1.i * i2.i); 
+        } else if (s1 is CstI i3 && (i3.i == 1 || i3.i == 0)) {
+            return i3.i == 1 ? s2: new CstI (0);
+        } else if (s2 is CstI i4 && (i4.i == 1 || i4.i == 0)) {
+            return i4.i == 1 ? s1: new CstI (0);;
+        } else {
+            return new Mul(s1, s2);
+        }
+    }
+}
+class Sub : Binop {
+    Expr _e1;
+    Expr _e2;
+    public Sub (Expr e1, Expr e2) {
+        _e1 = e1;
+        _e2 = e2;
+    }
+    public override string toString() {
+        return $"{_e1.toString()}-{_e2.toString()}";
+    }
+
+    public override int eval(List<(string, int)> env) {
+        return _e1.eval(env) - _e2.eval(env);
+    }
+
+    public override Expr simplify() {
+        var s1 = _e1.simplify();
+        var s2 = _e2.simplify();
+
+        if (s1 is CstI i1 && s2 is CstI i2) { 
+            return new CstI (i1.i - i2.i); 
+        } else if (s2 is CstI i3 && i3.i == 0) {
+            return s1;
+        } else {
+            return new Sub(s1, s2);
+        }
+    }
+}
+
