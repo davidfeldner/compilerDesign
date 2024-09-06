22113433f17226feb6f12a5ba6c85b55a5ed9334
Marius Nielsen <mawn@itu.dk>
2024-09-06 10:29:52 +0200

    began assignment2

---
+++ assignment2/2.4.fsx
@@ -0,0 +1,27 @@
+type sinstr =
+| SCstI of int                        (* push integer           *)
+| SVar of int                         (* push variable from env *)
+| SAdd                                (* pop args, push sum     *)
+| SSub                                (* pop args, push diff.   *)
+| SMul                                (* pop args, push product *)
+| SPop                                (* pop value/unbind var   *)
+| SSwap;;                             (* exchange top and next  *)
+
+(* Ex 2.4 - assemble to integers *)
+(* SCST = 0, SVAR = 1, SADD = 2, SSUB = 3, SMUL = 4, SPOP = 5, SSWAP = 6; *)
+let sinstrToInt = function
+  | SCstI i -> [0;i]
+  | SVar i  -> failwith "Not implemented"
+  | SAdd    -> failwith "Not implemented"
+  | SSub    -> failwith "Not implemented"
+  | SMul    -> failwith "Not implemented"
+  | SPop    -> failwith "Not implemented"
+  | SSwap   -> failwith "Not implemented"
+
+let assemble instrs = failwith "Not implemented"
+
+(* Output the integers in list inss to the text file called fname: *)
+
+let intsToFile (inss : int list) (fname : string) = 
+    let text = String.concat " " (List.map string inss)
+    System.IO.File.WriteAllText(fname, text);;
