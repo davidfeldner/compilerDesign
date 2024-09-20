b2cb863ed5675f5e5bb64769041cbee37a2eaaba
Marius Nielsen <mawn@itu.dk>
2024-09-20 10:50:39 +0200

    4.2 done

--- assignment4/4.2/Program.fs
+++ assignment4/4.2/Program.fs
@@ -1,11 +1,6 @@
-﻿open Parse
-open ParseAndRun
-
-let e1 = fromString "5+7"
-let e2 = fromString "let y = 7 in y + 2 end"
-let e3 = fromString "let f x = x + 7 in f 2 end"
+﻿open ParseAndRun
 
 let prog =
-    fromString "let sum n = if n > 0 then (sum (n-1) +n) else n in sum 1000 end"
+    fromString "let sum n = if n = 0 then n else (sum (n-1) +n) in sum 1000 end"
 
 printfn "%A" (run prog)
