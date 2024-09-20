9d8ae06955e4f05252078ed331b94e3d6f2ffacf
Marius Nielsen <mawn@itu.dk>
2024-09-20 11:15:13 +0200

    4.2 done 4real

--- assignment4/4.2/Program.fs
+++ assignment4/4.2/Program.fs
@@ -1,6 +1,34 @@
 ﻿open ParseAndRun
 
-let prog =
-    fromString "let sum n = if n = 0 then n else (sum (n-1) +n) in sum 1000 end"
+let a = fromString "let sum n = if n = 0 then n else (sum (n-1) +n) in sum 1000 end"
 
-printfn "%A" (run prog)
+let b =
+    fromString
+        @"
+    let pow expo = if expo = 1 then 3 else 3*(pow (expo-1)) in 
+        pow 8
+    end
+    "
+
+let c =
+    fromString
+        @"
+    let pow expo = if expo = 1 then 3 else 3*(pow (expo-1)) in
+        let sum n = if n = 0 then 1 else (sum (n-1) + pow n) in 
+            sum 11 
+        end
+    end
+    "
+
+//like this hack? xD
+let d =
+    fromString
+        @"
+let pow8 b = b*b*b*b*b*b*b*b in 
+    let sum n = if n = 0 then 0 else (sum (n-1) + pow8 n) in 
+        sum 10
+    end
+end
+"
+
+printfn "%A" (run d)
