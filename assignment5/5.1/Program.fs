a14e7fb322c2e1cbecc55571747c16dcd8672448
davidfeldner <davidfeldner12@gmail.com>
2024-09-27 10:18:43 +0200

    Start assignment5

---
+++ assignment5/5.1/Program.fs
@@ -0,0 +1,12 @@
+﻿let rec merge (lists: int list * int list): int list =
+    match lists with
+    | x::xs, y::xy -> if x <= y then x::y::(merge ) else y::x::acc
+    | x::xs, [] -> x::acc
+    | [], y::ys -> y::acc
+    | [], [] -> acc
+
+
+let l1 = [3;5;12]
+let l2 = [2;3;4;7]
+
+printfn "%A" (merge (l1, l2))
