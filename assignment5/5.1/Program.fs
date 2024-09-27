664110b6c53912db6d7a67e0003cd1cecc025938
davidfeldner <davidfeldner12@gmail.com>
2024-09-27 11:13:16 +0200

    5.1 fix

--- assignment5/5.1/Program.fs
+++ assignment5/5.1/Program.fs
@@ -1,12 +1,12 @@
 ﻿let rec merge (lists: int list * int list): int list =
     match lists with
-    | x::xs, y::xy -> if x <= y then x::y::(merge ) else y::x::acc
-    | x::xs, [] -> x::acc
-    | [], y::ys -> y::acc
-    | [], [] -> acc
+    | x::xs, y::xy -> if x <= y then x::(merge (xs, y::xy)) else y::(merge (x::xs, xy))
+    | x::xs, [] -> x::(merge (xs, []))
+    | [], y::ys -> y::(merge ([], ys))
+    | [], [] -> []
 
 
 let l1 = [3;5;12]
-let l2 = [2;3;4;7]
+let l2 = [2;3;4;7;8;9]
 
 printfn "%A" (merge (l1, l2))
