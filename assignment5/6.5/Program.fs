7d3d47181882f31cd73d2b33cc568168a95b837a
davidfeldner <davidfeldner12@gmail.com>
2024-10-08 23:13:11 +0200

    6.5 almost done

--- assignment5/6.5/Program.fs
+++ assignment5/6.5/Program.fs
@@ -1,5 +1,5 @@
 ﻿open ParseAndType
-
+// int
 let res1 = inferType(fromString "let f x = 1
 in f f end");;
 
@@ -8,6 +8,7 @@ in f f end");;
     inferType(fromString "let f g = g g
 in f end");; *)
 
+// bool
 let res3 =
     inferType(fromString "let f x =
 let g y = y
@@ -21,6 +22,7 @@ in f 42 end");;
     in g false end
 in f 42 end");; *)
 
+// bool
 let res5 =
     inferType(fromString "let f x =
     let g y = if true then y else x
@@ -28,13 +30,40 @@ let res5 =
 in f true end");;
 
 
-
-
-
-
-
 printfn "%A" res1
 //printfn "%A" res2
 printfn "%A" res3
 //printfn "%A" res4
 printfn "%A" res5
+
+
+//(bool -> bool)
+printfn "%A" (inferType(fromString "let f x = if x then true else x in f end"))
+//(int -> int)
+printfn "%A" (inferType(fromString "let f x = x + 1 in f end"))
+//(int -> (int -> int))
+printfn "%A" (inferType(fromString "let f x =
+        let g y = if true then y + 1 else x + 1
+        in g end
+    in f end"))
+//"('h -> ('g -> 'h))"
+printfn "%A" (inferType(fromString "let f x =
+    let g y = x
+    in g end
+in f end"))
+//"('g -> ('h -> 'h))"
+printfn "%A" (inferType(fromString "let f x =
+    let g y = y
+    in g end
+in f end"))
+
+//(’a -> ’b) -> (’b -> ’c) -> (’a -> ’c)
+printfn "%A" (inferType(fromString "
+let f a = a
+in let g b = b
+        let h c = c
+        in h end
+    in g end 
+end
+"))
+
