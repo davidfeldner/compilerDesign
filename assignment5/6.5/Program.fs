ff65b994ad3873a2d5cfb254ba9751ffe719764b
davidfeldner <davidfeldner12@gmail.com>
2024-10-08 22:46:44 +0200

    6.5 done?

--- assignment5/6.5/Program.fs
+++ assignment5/6.5/Program.fs
@@ -1,12 +1,40 @@
-﻿open ParseAndRunHigher
+﻿open ParseAndType
 
-let res1 = run (fromString "let add x = fun y -> x+y
-in add 2 5 end")
+let res1 = inferType(fromString "let f x = 1
+in f f end");;
 
-let res2 = run (fromString "let add = fun x -> fun y -> x+y
-in add 2 5 end")
+// "f g = g g" Here g takes itself as a parameter in the function declaration which makes it impossible to determine its type
+(* let res2 =
+    inferType(fromString "let f g = g g
+in f end");; *)
+
+let res3 =
+    inferType(fromString "let f x =
+let g y = y
+in g false end
+in f 42 end");;
+
+// The if statment returns 2 different types, y is an int, since f is called with 42, and x is a boolean, since g is called with false.
+(* let res4 =
+    inferType(fromString "let f x =
+    let g y = if true then y else x
+    in g false end
+in f 42 end");; *)
+
+let res5 =
+    inferType(fromString "let f x =
+    let g y = if true then y else x
+    in g false end
+in f true end");;
+
+
+
+
 
 
 
 printfn "%A" res1
-printfn "%A" res2
+//printfn "%A" res2
+printfn "%A" res3
+//printfn "%A" res4
+printfn "%A" res5
