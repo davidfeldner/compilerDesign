6fce96d00a1116b2408b466e18f4a328be9a9c15
davidfeldner <davidfeldner12@gmail.com>
2024-10-08 22:32:57 +0200

    6.4 and start 6.5

---
+++ assignment5/6.5/Program.fs
@@ -0,0 +1,12 @@
+﻿open ParseAndRunHigher
+
+let res1 = run (fromString "let add x = fun y -> x+y
+in add 2 5 end")
+
+let res2 = run (fromString "let add = fun x -> fun y -> x+y
+in add 2 5 end")
+
+
+
+printfn "%A" res1
+printfn "%A" res2
